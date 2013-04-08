0badd7b06e6d419790da6b63d4848b154319f114
Lee Henson <lee.m.henson@gmail.com>
2013-04-08 14:01:15 +0100

    parallelpoolfiber

---
+++ src/Retlang/Fibers/ParallelPoolFiber.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Retlang.Core;
+
+namespace Retlang.Fibers
+{
+	/// <summary>
+	/// Similar to PoolFiber, but doesn't allocate batches of messages to ThreadPool workers, 
+	/// instead it tries to allocate a single message to a single worker, potentially increasing 
+	/// the rate at which messages are processed.
+	/// </summary>
+	public class ParallelPoolFiber : IFiber
+	{
+		readonly ConcurrentQueue<Action> _queue = new ConcurrentQueue<Action>();
+		readonly Subscriptions _subscriptions = new Subscriptions();
+
+		readonly IExecutor _executor;
+		readonly IThreadPool _pool;
+		readonly Scheduler _timer;
+
+		ExecutionState _started = ExecutionState.Created;
+
+		/// <summary>
+		/// Construct new instance.
+		/// </summary>
+		public ParallelPoolFiber() : this(new DefaultThreadPool(), new DefaultExecutor())
+		{
+		}
+
+		/// <summary>
+		/// Construct new instance.
+		/// </summary>
+		public ParallelPoolFiber(IThreadPool pool, IExecutor executor)
+		{
+			_pool = pool;
+			_executor = executor;
+			_timer = new Scheduler(this);
+		}
+
+		/// <summary>
+		/// Stops the fiber.
+		/// </summary>
+		public void Dispose()
+		{
+			Stop();
+		}
+
+		/// <summary>
+		/// Enqueue a single action.
+		/// </summary>
+		/// <param name="action"></param>
+		public void Enqueue(Action action)
+		{
+			if (_started == ExecutionState.Stopped) return;
+
+			for(var i = 0; i < _queue.Count; i++)
+				_pool.Queue(ExecuteNextAction);
+
+			_queue.Enqueue(action);
+
+			if (_started == ExecutionState.Created) return;
+
+			_pool.Queue(ExecuteNextAction);
+		}
+
+		void ExecuteNextAction(object state)
+		{
+			Action action;
+
+			if (!_queue.TryDequeue(out action)) return;
+
+			_executor.Execute(action);
+		}
+		
+		/// <summary>
+		/// Gets the queue size
+		/// </summary>
+		public int QueueSize
+		{
+			get { return _queue.Count; }
+		}
+
+		/// <summary>
+		/// Start consuming actions.
+		/// </summary>
+		public void Start()
+		{
+			if (_started == ExecutionState.Running)
+			{
+				throw new ThreadStateException("Already Started");
+			}
+
+			_started = ExecutionState.Running;
+		}
+
+		/// <summary>
+		/// Stop consuming actions.
+		/// </summary>
+		public void Stop()
+		{
+			_timer.Dispose();
+			_started = ExecutionState.Stopped;
+			_subscriptions.Dispose();
+		}
+
+		///<summary>
+		/// Register subscription to be unsubcribed from when the fiber is disposed.
+		///</summary>
+		///<param name="toAdd"></param>
+		public void RegisterSubscription(IDisposable toAdd)
+		{
+			_subscriptions.Add(toAdd);
+		}
+
+		///<summary>
+		/// Deregister a subscription.
+		///</summary>
+		///<param name="toRemove"></param>
+		///<returns></returns>
+		public bool DeregisterSubscription(IDisposable toRemove)
+		{
+			return _subscriptions.Remove(toRemove);
+		}
+
+		/// <summary>
+		/// <see cref="IScheduler.Schedule(Action,long)"/>
+		/// </summary>
+		/// <param name="action"></param>
+		/// <param name="firstInMs"></param>
+		/// <returns></returns>
+		public IDisposable Schedule(Action action, long firstInMs)
+		{
+			return _timer.Schedule(action, firstInMs);
+		}
+
+		/// <summary>
+		/// <see cref="IScheduler.ScheduleOnInterval(Action,long,long)"/>
+		/// </summary>
+		/// <param name="action"></param>
+		/// <param name="firstInMs"></param>
+		/// <param name="regularInMs"></param>
+		/// <returns></returns>
+		public IDisposable ScheduleOnInterval(Action action, long firstInMs, long regularInMs)
+		{
+			return _timer.ScheduleOnInterval(action, firstInMs, regularInMs);
+		}
+	}
+}
---
+++ src/RetlangTests/ParallelPoolFiberTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using Retlang.Core;
+using Retlang.Fibers;
+using Rhino.Mocks;
+using Timer = System.Timers.Timer;
+
+namespace RetlangTests
+{
+	[TestFixture]
+	public class ParallelPoolFiberTests
+	{
+		public class AsyncBlock
+		{
+			
+		}
+
+		[Test]
+		public void EnqueueSingleActionOnParallelPoolFiber()
+		{
+			var fiber = new ParallelPoolFiber();
+			fiber.Start();
+
+			var executed = false;
+			var timeout = false;
+			
+			var timer = new Timer {AutoReset = false, Enabled = false, Interval = 100};
+			timer.Elapsed += (sender, args) => timeout = true;
+			timer.Start();
+			
+			var action = new Action(() => executed = true);
+			fiber.Enqueue(action);
+
+			while(!timeout) {}
+			
+			Assert.IsTrue(executed);
+		}
+
+		//[Test]
+		//public void ActionsAreOnlyEnqueuedWhileTheFiberIsNotStopped()
+		//{
+		//    var repo = new MockRepository();
+		//    var pool = repo.CreateMock<IThreadPool>();
+		//    var timeout = new ManualResetEventSlim();
+
+		//    var fiber = new ParallelPoolFiber(pool, new RetlangTests.StubExecutor());
+			
+		//    //var action1 = new Action(() => {});
+		//    //var action2 = new Action(() => {});
+
+		//    var action1Executed = false;
+		//    var action2Executed = false;
+
+		//    fiber.Enqueue(() => action1Executed = true);
+
+		//    timeout.Wait(TimeSpan.FromMilliseconds(100));
+		//}
+
+		//public class StubExecutor : IExecutor
+		//{
+		//    public void Execute(Queue<Action> toExecute)
+		//    {
+		//    }
+
+		//    public void Execute(Action toExecute)
+		//    {
+		//    }
+		//}
+	}
+}
